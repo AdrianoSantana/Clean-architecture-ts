5ff5743658d2042f57a1fa1125b0d1572d888885
ans3 <ans3@cin.ufpe.br>
2021-02-02 15:56:43 -0300

    test: signupcontroller retorna 400 se nenhuma senha for passada

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,9 +17,7 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
-})
 
-describe('Signup Controller', () => {
   test('Deve retornar 400 se nenhum email for enviado', () => {
     const sut = new SignUpController()
     const httpRequest = {
@@ -32,5 +30,19 @@ describe('Signup Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Deve retornar 400 se nenhuma senha for enviado', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 })
