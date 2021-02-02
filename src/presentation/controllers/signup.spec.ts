6b829fb397245d26e4bd3c9973841623c9231687
ans3 <ans3@cin.ufpe.br>
2021-02-02 10:53:29 -0300

    test: signup controller retorna um error se nenhum email for passado

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,3 +17,19 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
+
+describe('Signup Controller', () => {
+  test('Deve retornar 400 se nenhum email for enviado', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
+})
