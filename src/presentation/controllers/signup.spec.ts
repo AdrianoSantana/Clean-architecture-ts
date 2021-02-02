9bc9288a431e313aabe24002aafd4026df808b3b
ans3 <ans3@cin.ufpe.br>
2021-02-02 10:40:26 -0300

    test: cadastroController retorna com erro 400 se nenhum nome for retornado

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+'use strict'
+
+import { SignUpController } from './signup'
+
+describe('Signup Controller', () => {
+  test('Deve retornar 400 se nenhum nome for enviado', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
