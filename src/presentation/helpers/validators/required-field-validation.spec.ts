1523d864fe1aba69f9ca8d1d5e770f7b439e55e0
ans3 <ans3@cin.ufpe.br>
2021-02-25 07:19:48 -0300

    test: ensure required-field-validation returns false if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -9,4 +9,12 @@ describe('Required field validation', () => {
     })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate({
+      any_field: 'field'
+    })
+    expect(error).toBeFalsy()
+  })
 })
