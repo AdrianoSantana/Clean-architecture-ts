ddaed2f40c2416e49452e2c8652f2994b22eb1d0
ans3 <ans3@cin.ufpe.br>
2021-03-09 12:05:57 -0300

    refactor: remove duplicated code

--- src/infra/criptography/bcrypt-adapter/bcripty-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcripty-adapter.ts
@@ -3,9 +3,7 @@ import { HashComparer } from '../../../data/protocols/criptography/hash-compare'
 import { Hasher } from '../../../data/protocols/criptography/hasher'
 
 export class BcryptAdapter implements Hasher, HashComparer {
-  private readonly salt: number
-  constructor (salt: number) {
-    this.salt = salt
+  constructor (private readonly salt: number) {
   }
 
   async compare (value: string, hash: string): Promise<boolean> {
--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -3,16 +3,10 @@ import { Controller, HttpRequest, HttpResponse, AddAccount } from './signupProto
 import { Validation } from '../../protocols/validation'
 
 export class SignUpController implements Controller {
-  private readonly addAccount: AddAccount
-  private readonly validation: Validation
-
   constructor (
-    addAccount: AddAccount,
-    validation: Validation
-  ) {
-    this.addAccount = addAccount
-    this.validation = validation
-  }
+    private readonly addAccount: AddAccount,
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
