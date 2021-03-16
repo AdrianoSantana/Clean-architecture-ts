6059a7183d7065c5d45fa059e03a87dc74feedb5
ans3 <ans3@cin.ufpe.br>
2021-03-16 15:47:03 -0300

    feat: add banco

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://ans3:679852@cluster0.7qew5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.port || 5050,
   jwtSecret: process.env.JWT_SECRET || 'th47&6a@5bjqo1'
 }
