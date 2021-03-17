503aec986722aa0b5382a8df89c7fcb3c10f7ed3
ans3 <ans3@cin.ufpe.br>
2021-03-17 06:07:55 -0300

    refactor: changed message connect

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,6 +4,6 @@ import env from './config/env'
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(env.port, () => console.log('Server running at http://localhost:5050'))
+    app.listen(env.port, () => console.log(`Server running on port ${env.port}`))
   })
   .catch(console.error)
